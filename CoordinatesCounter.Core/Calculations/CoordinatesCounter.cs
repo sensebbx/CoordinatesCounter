968a170a9adde161aa381edf3fd257f8f70ee4c4
sensebbx <33457416+sensebbx@users.noreply.github.com>
2019-06-03 10:11:44 +0300

    Added documentation to all classes

--- CoordinatesCounter.Core/Calculations/CoordinatesCounter.cs
+++ CoordinatesCounter.Core/Calculations/CoordinatesCounter.cs
@@ -3,26 +3,59 @@ using CoordinatesCounter.Core.InputStructs;
 
 namespace CoordinatesCounter.Core.Calculations
 {
+    /// <summary>
+    /// Contains main calculating functions
+    /// </summary>
     public class CoordinatesCounter
     {
+        /// <summary>
+        /// Contains final camera coordinates relatively to Earth
+        /// </summary>
         private AngleCameraRegEarth _angleCameraRegEarth;
 
+        /// <summary>
+        /// Contains aircraft parameters
+        /// </summary>
         private readonly AircraftIpnutData _aircraftIpnutData;
+
+        /// <summary>
+        /// Contains camera parameters
+        /// </summary>
         private readonly CameraInputData _cameraInputData;
 
+        /// <summary>
+        /// Output of agorithm, Y coordinate of object in "CK-90"
+        /// </summary>
         private float _x;
+
+        /// <summary>
+        /// Output of algorithm, Y coordinate of object in  "CK-90"
+        /// </summary>
         private float _y;
 
+        /// <summary>
+        /// X coordinate of object in "CK-90"
+        /// </summary>
         public float X
         {
             get { return _x; }
         }
 
+        /// <summary>
+        /// Y coordinate of object in "CK-90"
+        /// </summary>
         public float Y
         {
             get { return _y; }
         }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="anglePlaneMatrix">Contains plane angle matrix</param>
+        /// <param name="angleCameraOnPlaneMatrix">Contains camera angle matrix</param>
+        /// <param name="cameraInputData">Contains camera input parameters</param>
+        /// <param name="aircraftIpnutData">Contains aircraft input parameters</param>
         public CoordinatesCounter(ref AnglePlaneMatrix anglePlaneMatrix,
             ref AngleCameraOnPlaneMatrix angleCameraOnPlaneMatrix,
             ref CameraInputData cameraInputData,
@@ -34,6 +67,10 @@ namespace CoordinatesCounter.Core.Calculations
             _aircraftIpnutData = aircraftIpnutData;
         }
 
+        /// <summary>
+        /// Main core function that calculating coordinates of objects
+        /// </summary>
+        /// <param name="objectInputData">Input object data</param>
         public void Calculate(ObjectInputData objectInputData)
         {
             _x = (float) (_aircraftIpnutData.XS -
--- CoordinatesCounter.Core/Matrixes/AngleCameraOnPlaneMatrix.cs
+++ CoordinatesCounter.Core/Matrixes/AngleCameraOnPlaneMatrix.cs
@@ -3,15 +3,30 @@ using Accord.Math;
 
 namespace CoordinatesCounter.Core
 {
+    /// <summary>
+    /// Contains camera angle matrix relatively to aircraft
+    /// </summary>
     public class AngleCameraOnPlaneMatrix
     {
+        /// <summary>
+        /// Angle matrix of camera
+        /// </summary>
         private Accord.Math.Matrix3x3 _matrix;
 
+        /// <summary>
+        /// Angle matrix of camera
+        /// </summary>
         public Accord.Math.Matrix3x3 Matrix
         {
             get { return _matrix; }
         }
-        
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="phi">Angle coordinate</param>
+        /// <param name="theta">Angle coordinate</param>
+        /// <param name="gamma">Angle coordinate</param>
         public AngleCameraOnPlaneMatrix(double phi, double theta, double gamma)
         {
             if (phi == 0 && theta == 0 && gamma == 0)
@@ -30,7 +45,7 @@ namespace CoordinatesCounter.Core
                 };
                 return;
             }
-            
+
             _matrix = new Matrix3x3
             {
                 V00 = (float) (Math.Cos(phi) * Math.Cos(theta)),
@@ -38,11 +53,11 @@ namespace CoordinatesCounter.Core
                 V02 = (float) (-Math.Sin(phi) * Math.Sin(gamma) - Math.Cos(phi) * Math.Sin(theta) * Math.Cos(gamma)),
                 V10 = (float) (Math.Sin(phi) * Math.Cos(theta)),
                 V11 = (float) (-Math.Cos(phi) * Math.Cos(gamma) - Math.Sin(phi) * Math.Sin(theta) * Math.Sin(gamma)),
-                V12 = (float) (Math.Cos(phi)*Math.Sin(gamma) - Math.Sin(phi)*Math.Sin(theta)*Math.Cos(gamma)),
+                V12 = (float) (Math.Cos(phi) * Math.Sin(gamma) - Math.Sin(phi) * Math.Sin(theta) * Math.Cos(gamma)),
                 V20 = (float) (Math.Sin(theta)),
-                V21 = (float)( Math.Cos(theta)*Math.Sin(gamma)),
-                V22 = (float) (Math.Cos(theta)*Math.Cos(gamma))
-            };   
+                V21 = (float) (Math.Cos(theta) * Math.Sin(gamma)),
+                V22 = (float) (Math.Cos(theta) * Math.Cos(gamma))
+            };
         }
     }
 }
